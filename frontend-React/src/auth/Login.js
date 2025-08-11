628054226d6ec3549c7f7419f1db53e1a6982df7
Abdel-rahmanMahmoud <aboiskandar1@gmail.com>
2025-08-11 21:42:20 +0300

    Update Api Url

--- frontend-React/src/auth/Login.js
+++ frontend-React/src/auth/Login.js
@@ -7,6 +7,7 @@ import '../styles/auth.css';
 
 const REACT_APP_API_BASE_URL = process.env.REACT_APP_REACT_APP_API_BASE_URL ;
 console.log(`REACT_APP_API_BASE_URL: ${REACT_APP_API_BASE_URL}`);
+console.log(process.env);
 
 
 function Login() {
