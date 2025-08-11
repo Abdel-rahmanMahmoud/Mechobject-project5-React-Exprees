9837037c0e8074ac74c54c7d037f1d56a6d9f2a9
Abdel-rahmanMahmoud <aboiskandar1@gmail.com>
2025-08-11 18:13:57 +0300

    Update Cors

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ sequelize.authenticate()
   });
 
 app.use(cors({
-  origin: true ,
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
   credentials: true
 }));
 app.use(cookieParser());
